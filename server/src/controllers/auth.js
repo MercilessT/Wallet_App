99ca18811e38e3f3073ee5fb9ec90c55d0d58988
Yurii Voitko <104502294+MercilessT@users.noreply.github.com>
2024-01-20 00:24:45 +0200

    Update auth.js
    add comments

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -4,17 +4,23 @@ import jwt from 'jsonwebtoken'
 import { createError } from '../error.js'
 
 
+/**
+ * User registration endpoint
+ */
 export const signup = async (req, res, next) => {
   try {
+    // Check if email and password are provided
     if (!req.body.email || !req.body.password) {
       return next(createError(400, 'Email and password are required for registration...'))
     }
 
     const user = await User.findOne({ email: req.body.email })
+    // Check if user with the same email already exists
     if (user) {
       return next(createError(409, 'User with this email already exists...'))
     }
 
+    // Hash the password before saving the new user
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(req.body.password, salt)
 
@@ -29,29 +35,38 @@ export const signup = async (req, res, next) => {
 }
 
 
+/**
+ * User login endpoint
+ */
 export const login = async (req, res, next) => {
   try {
+    // Check if email and password are provided
     if (!req.body.email || !req.body.password) {
       return next(createError(400, 'Email and password are required for registration...'))
     }
 
     const user = await User.findOne({ email: req.body.email })
 
+    // Check if the user exists
     if (!user) {
       return next(createError(404, 'User not found...'))
     }
 
+    // Compare the provided password with the hashed password in the database
     const isCorrect = await bcrypt.compare(
       req.body.password,
       user.password,
     )
 
+    // If the passwords don't match, return an error
     if (!isCorrect) {
       return next(createError(400, 'Wrong credentials...'))
     }
 
+    // Assign a JWT token to the user
     const token = jwt.sign({ id: user._id }, process.env.JWT)
 
+    // Omit the password and send the user data along with the token in a cookie
     const { password, ...others } = user.toJSON()
 
     res.cookie('access_token', token, {
@@ -64,8 +79,12 @@ export const login = async (req, res, next) => {
 }
 
 
+/**
+ * User logout endpoint
+ */
 export const logout = async (req, res, next) => {
   try {
+    // Clear the access_token cookie to log the user out
     res.clearCookie('access_token')
     res.send('Logged out successfully!')
 
